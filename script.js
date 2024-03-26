edaf4b2f220f8801e0f6a681800d0cd2ae40ce89
AnnafYousuf <annafyousuf1@gmail.com>
2024-03-26 12:11:59 +0500

    Assignment Done

---
+++ script.js
@@ -0,0 +1,9 @@
+const turnon =()=>{
+    let imgob=document.getElementById("bulb");
+    imgob.setAttribute("src", "231231.jpg");
+};
+
+const turnoff =()=>{
+    let imgob=document.getElementById("bulb");
+    imgob.setAttribute("src", "123123.jpeg");
+};
